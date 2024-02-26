4495c37c431bff8c38866d3c9a0ba209865c5e33
Leonardo Vaz <leonardovazgremista@hotmail.com>
2024-02-26 10:45:57 -0300

    last commit/1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { ShowCardComponent } from './components/show-card/show-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, ShowCardComponent, SmallCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
