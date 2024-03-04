0f2128c1a048951c5146775a73380feef10dbb8a
Nyelan <leonardovazgremista@hotmail.com>
2024-03-04 17:13:12 -0300

    small-card att

--- src/app/components/small-card/card-tape/card-tape.component.ts
+++ src/app/components/small-card/card-tape/card-tape.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-tape',
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './card-tape.component.css'
 })
 export class CardTapeComponent {
-
+  @Input()
+  title:string = '';
+  @Input()
+  desc:string = '';
+  @Input()
+  img:string = '';
 }
